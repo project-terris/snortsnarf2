f10a79cb2d5898a9f2698a1d4f0ba1c692c9947a
bensoer <ben@soernet.ca>
2016-04-10 14:03:15 -0700

    refactored project. added output folder to make output also dynamic. put in system to return IReader

---
+++ lib/ex/WriterException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bensoer
+ * Date: 10/04/16
+ * Time: 1:52 PM
+ */
+class WriterException extends Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        // some code
+
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
+
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
---
+++ lib/output/WriterFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bensoer
+ * Date: 10/04/16
+ * Time: 1:48 PM
+ */
+class WriterFactory
+{
+
+    public static function determineOutput(ArgParcer $argument){
+
+        return null;
+    }
+}
